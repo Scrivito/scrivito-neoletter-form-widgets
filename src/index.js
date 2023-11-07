4b71d9215ff42cd2417c221513ca52a1c9a99b72
Amier El Ahmad <amier.elahmad@justrelate.com>
2023-11-07 10:27:46 +0100

    export helper functions for custom made form widgets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { initScrivitoFormWidgets } from "./config/scrivitoConfig";
+import { insideFormContainerValidation } from "./Widgets/FormContainerWidget/utils/validations/insideFormContainerValidation";
+import { getFormContainer } from "./Widgets/FormContainerWidget/utils/getFormContainer";
+import { customFieldNameValidation } from "./Widgets/FormContainerWidget/utils/validations/customFieldNameValidation";
 
-export { initScrivitoFormWidgets };
+export { initScrivitoFormWidgets, insideFormContainerValidation, getFormContainer, customFieldNameValidation };
