782dac9e903acba947643297c578ba4997afc762
Alexander Pepper <alexander.pepper@justrelate.com>
2024-07-25 11:41:55 +0200

    IconColor: Use "!important"
    Otherwise in some styles the color gets lost.

--- src/Widgets/FormStepContainerWidget/utils/getIconColor.ts
+++ src/Widgets/FormStepContainerWidget/utils/getIconColor.ts
@@ -4,12 +4,12 @@ export function getIconColor(widget: Widget): string {
   const type = widget.get("colorType");
   switch (type) {
     case "custom":
-      return widget.get("customColor") as string;
+      return `${widget.get("customColor")} !important`;
     case "secondary":
-      return "var(--bs-secondary, #f03a47)";
+      return "var(--bs-secondary, #f03a47) !important";
     case "primary":
-      return "var(--bs-primary, #5c9dcd)";
+      return "var(--bs-primary, #5c9dcd) !important";
     default:
-      return "gold";
+      return "gold !important";
   }
 }
