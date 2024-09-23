d169d9fef1d08f657c6709ec3ab13caf8b0d6ad5
Amier El Ahmad <amier.elahmad@justrelate.com>
2024-09-23 14:54:07 +0200

    add new tests

--- test/tests/Widgets/components/FormSubmissionFailed.test.tsx
+++ test/tests/Widgets/components/FormSubmissionFailed.test.tsx
@@ -1,11 +1,57 @@
 import * as React from "react";
-import { render } from "@testing-library/react";
+import * as Scrivito from "scrivito";
+import { screen, render } from "@testing-library/react";
 import { FormSubmissionFailed } from "../../../../src/Widgets/FormStepContainerWidget/components/FormSubmissionFailedComponent";
 import renderer from "react-test-renderer";
 import { DummyWidget } from "../../../helpers/dummyWidget";
 import { Widget } from "scrivito";
+import { FormDateWidget } from "../../../../src/Widgets/FormDateWidget/FormDateWidgetClass";
+import { FormStepContainerWidget } from "../../../../src/Widgets/FormStepContainerWidget/FormStepContainerWidgetClass";
+import "../../../../src/Widgets/FormStepContainerWidget/FormStepContainerWidgetComponent";
+import "../../../../src/Widgets/FormDateWidget/FormDateWidgetComponent";
+import PageRenderer from "../../../helpers/pageRenderer";
 
+Scrivito.configure({ tenant: "inMemory" });
 const submissionFailureText = "Submission failed!";
+
+const formProps = {
+  formId: "test-id",
+  failedMessage: "failed",
+  submittedMessage: "submitted",
+  submittingMessage: "submitting",
+  hiddenFields: [],
+  formType: "single-step",
+  steps: [],
+  forwardButtonText: "forward",
+  backwardButtonText: "back",
+  submitButtonText: "submit",
+  showBorder: false,
+  showCaptcha: false,
+  captchaAlignment: "center",
+  showReview: true,
+  includeEmptyAnswers: false,
+  showStepsInReview: false,
+  showReviewHeader: false,
+  showReviewFooter: false,
+  reviewButtonText: "Review",
+  reviewHeaderTitle: "Review header",
+  reviewCloseButtonText: "Close",
+  singleSubmitButtonAlignment: "left",
+  previewSubmittingMessage: false,
+  previewFailedMessage: false,
+  previewSubmittedMessage: false,
+  submittingMessageType: "default",
+  submittedMessageType: "default",
+  failedMessageType: "default",
+  submittedMessageWidgets: [],
+  submittingMessageWidgets: [],
+  failedMessageWidgets: [],
+  showRetryButton: false,
+  retryButtonText: "retry"
+};
+
+const pageRenderer = new PageRenderer();
+
 describe("FormSubmissionFailed", () => {
   const widgetProps = {
     submissionFailureText: submissionFailureText,
@@ -33,6 +79,108 @@ describe("FormSubmissionFailed", () => {
 
     expect(textElement).toBeInTheDocument();
     expect(textElement.tagName.toLowerCase()).toBe("span");
+  });
+
+  it("renders the widget list without a retry button if edit mode is true", () => {
+    jest.spyOn(Scrivito, "isInPlaceEditingActive").mockReturnValue(true);
+    const submissionWidgets = [
+      new FormDateWidget({
+        customFieldName: "custom_date",
+        title: "date",
+        dateType: "date"
+      })
+    ];
+
+    pageRenderer.render({
+      body: [new FormStepContainerWidget({ ...formProps, failedMessageWidgets: submissionWidgets, failedMessageType: "widget-list", previewFailedMessage: true })]
+    });
+
+    const submissionContainer = document.querySelector(".form-submission-failed");
+    const textContainer = document.querySelector(".text-center");
+    const dateWidget = document.querySelector('input[type="date"]');
+    const retryButton = document.querySelector("button");
+    expect(textContainer).toBeNull();
+    expect(dateWidget).toBeInTheDocument();
+    expect(submissionContainer).toBeInTheDocument();
+    expect(retryButton).not.toBeInTheDocument();
+  });
+
+  it("does not render if edit mode is false", () => {
+    jest.spyOn(Scrivito, "isInPlaceEditingActive").mockReturnValue(false);
+
+    const submissionWidgets = [
+      new FormDateWidget({
+        customFieldName: "custom_date",
+        title: "date",
+        dateType: "date"
+      })
+    ];
+
+    pageRenderer.render({
+      body: [new FormStepContainerWidget({ ...formProps, failedMessageWidgets: submissionWidgets, failedMessageType: "widget-list", previewFailedMessage: true })]
+    });
+
+    const textContainer = document.querySelector(".text-center");
+    const dateWidget = document.querySelector('input[type="date"]');
+    const form = document.getElementById("test-id");
+
+    expect(textContainer).toBeNull();
+    expect(dateWidget).not.toBeInTheDocument();
+    expect(form).toBeInTheDocument();
+  });
+
+  it("does not render if preview is false and edit mode is true", () => {
+    jest.spyOn(Scrivito, "isInPlaceEditingActive").mockReturnValue(false);
+
+    const submissionWidgets = [
+      new FormDateWidget({
+        customFieldName: "custom_date",
+        title: "date",
+        dateType: "date"
+      })
+    ];
+
+    pageRenderer.render({
+      body: [new FormStepContainerWidget({ ...formProps, failedMessageWidgets: submissionWidgets, failedMessageType: "widget-list", previewFailedMessage: false })]
+    });
+
+    const textContainer = document.querySelector(".text-center");
+    const spanElement = screen.queryByText(submissionFailureText);
+    const dateWidget = document.querySelector('input[type="date"]');
+    const form = document.getElementById("test-id");
+
+    expect(textContainer).toBeNull();
+    expect(spanElement).not.toBeInTheDocument();
+    expect(dateWidget).not.toBeInTheDocument();
+    expect(form).toBeInTheDocument();
+  });
+
+  it("shows the retry button ", () => {
+    jest.spyOn(Scrivito, "isInPlaceEditingActive").mockReturnValue(true);
+
+    const submissionWidgets = [
+      new FormDateWidget({
+        customFieldName: "custom_date",
+        title: "date",
+        dateType: "date"
+      })
+    ];
+
+    pageRenderer.render({
+      body: [new FormStepContainerWidget({ ...formProps, failedMessageWidgets: submissionWidgets, failedMessageType: "widget-list", previewFailedMessage: true, showRetryButton: true })]
+    });
+
+    const textContainer = document.querySelector(".text-center");
+    const spanElement = screen.queryByText(submissionFailureText);
+    const dateWidget = document.querySelector('input[type="date"]');
+    const form = document.getElementById("test-id");
+    const retryButton = screen.getByRole("button");
+    expect(textContainer).not.toBeInTheDocument();
+    expect(spanElement).not.toBeInTheDocument();
+    expect(dateWidget).toBeInTheDocument();
+    expect(form).not.toBeInTheDocument();
+    expect(retryButton).toBeInTheDocument();
+    expect(retryButton).toHaveTextContent("retry");
   });
 
   it("renders correctly", () => {
--- test/tests/Widgets/components/FormSubmitting.test.tsx
+++ test/tests/Widgets/components/FormSubmitting.test.tsx
@@ -1,20 +1,56 @@
 import React from "react";
+import * as Scrivito from "scrivito";
 import { Widget } from "scrivito";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { FormSubmitting } from "../../../../src/Widgets/FormStepContainerWidget/components/FormSubmittingComponent";
 import renderer from "react-test-renderer";
 import { DummyWidget } from "../../../helpers/dummyWidget";
+import { FormDateWidget } from "../../../../src/Widgets/FormDateWidget/FormDateWidgetClass";
+import { FormStepContainerWidget } from "../../../../src/Widgets/FormStepContainerWidget/FormStepContainerWidgetClass";
+import "../../../../src/Widgets/FormStepContainerWidget/FormStepContainerWidgetComponent";
+import "../../../../src/Widgets/FormDateWidget/FormDateWidgetComponent";
+import PageRenderer from "../../../helpers/pageRenderer";
 
+Scrivito.configure({ tenant: "inMemory" });
 
 const submittingText = "Submitting...";
-describe("FormSubmitting", () => {
-  const widgetProperties = {
-    submittingText: "Submitting...",
-    type: "default",
-    submittingMessageWidgets: []
-  };
+const widgetProperties = {
+  submittingText: submittingText,
+  type: "default",
+  submittingMessageWidgets: []
+};
+const widget = new DummyWidget(widgetProperties) as unknown as Widget;
 
-  const widget = new DummyWidget(widgetProperties) as unknown as Widget;
+const formProps = {
+  formId: "test-id",
+  failedMessage: "failed",
+  submittedMessage: "submitted",
+  submittingMessage: "submitting",
+  hiddenFields: [],
+  formType: "single-step",
+  steps: [],
+  forwardButtonText: "forward",
+  backwardButtonText: "back",
+  submitButtonText: "submit",
+  showBorder: false,
+  showCaptcha: false,
+  captchaAlignment: "center",
+  showReview: true,
+  includeEmptyAnswers: false,
+  showStepsInReview: false,
+  showReviewHeader: false,
+  showReviewFooter: false,
+  reviewButtonText: "Review",
+  reviewHeaderTitle: "Review header",
+  reviewCloseButtonText: "Close",
+  singleSubmitButtonAlignment: "left",
+  previewSubmittingMessage: false
+};
+
+const pageRenderer = new PageRenderer();
+
+describe("FormSubmitting", () => {
+
   it("renders the spinner icon and outputs the submitting text", () => {
     const { container, getByText } = render(
       <FormSubmitting
@@ -33,23 +69,78 @@ describe("FormSubmitting", () => {
     expect(textElement).toBeInTheDocument();
     expect(textElement.tagName.toLowerCase()).toBe("span");
   });
-  it("renders the widget list", () => {
-    const { container, getByText } = render(
-      <FormSubmitting
-        submittingText={submittingText}
-        type="default"
-        widget={widget}
-      />
-    );
 
-    const icon = container.querySelector(".bi-arrow-repeat");
-    const textElement = getByText(submittingText);
+  it("renders the widget list if edit mode is true", () => {
+    jest.spyOn(Scrivito, "isInPlaceEditingActive").mockReturnValue(true);
+    const submissionWidgets = [
+      new FormDateWidget({
+        customFieldName: "custom_date",
+        title: "date",
+        dateType: "date"
+      })
+    ];
 
-    expect(icon).toBeInTheDocument();
-    expect(icon?.tagName.toLowerCase()).toBe("i");
+    pageRenderer.render({
+      body: [new FormStepContainerWidget({ ...formProps, submittingMessageWidgets: submissionWidgets, submittingMessageType: "widget-list", previewSubmittingMessage: true })]
+    });
 
-    expect(textElement).toBeInTheDocument();
-    expect(textElement.tagName.toLowerCase()).toBe("span");
+    const textContainer = document.querySelector(".text-center");
+    const dateWidget = document.querySelector('input[type="date"]');
+
+    expect(textContainer).toBeNull();
+    expect(dateWidget).toBeInTheDocument();
+  });
+
+  it("does not render if edit mode is false", () => {
+    jest.spyOn(Scrivito, "isInPlaceEditingActive").mockReturnValue(false);
+
+    const submissionWidgets = [
+      new FormDateWidget({
+        customFieldName: "custom_date",
+        title: "date",
+        dateType: "date"
+      })
+    ];
+
+    pageRenderer.render({
+      body: [new FormStepContainerWidget({ ...formProps, submittingMessageWidgets: submissionWidgets, submittingMessageType: "widget-list", previewSubmittingMessage: true })]
+    });
+
+    const textContainer = document.querySelector(".text-center");
+    const dateWidget = document.querySelector('input[type="date"]');
+    const form = document.getElementById("test-id");
+
+
+    expect(textContainer).toBeNull();
+    expect(dateWidget).not.toBeInTheDocument();
+    expect(form).toBeInTheDocument();
+  });
+
+
+  it("does not render if preview is false and edit mode is true", () => {
+    jest.spyOn(Scrivito, "isInPlaceEditingActive").mockReturnValue(false);
+
+    const submissionWidgets = [
+      new FormDateWidget({
+        customFieldName: "custom_date",
+        title: "date",
+        dateType: "date"
+      })
+    ];
+
+    pageRenderer.render({
+      body: [new FormStepContainerWidget({ ...formProps, submittingMessageWidgets: submissionWidgets, submittingMessageType: "widget-list", previewSubmittingMessage: false })]
+    });
+
+    const textContainer = document.querySelector(".text-center");
+    const spanElement = screen.queryByText(submittingText);
+    const dateWidget = document.querySelector('input[type="date"]');
+    const form = document.getElementById("test-id");
+
+    expect(textContainer).toBeNull();
+    expect(spanElement).not.toBeInTheDocument();
+    expect(dateWidget).not.toBeInTheDocument();
+    expect(form).toBeInTheDocument();
   });
 
   it("renders correctly", () => {
