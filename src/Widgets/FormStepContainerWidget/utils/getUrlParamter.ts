53f2d65808a2e6a7138e5fcf69718388f909b6ba
Amier El Ahmad <amier.elahmad@justrelate.com>
2026-04-13 13:46:43 +0200

    Prerender fix

--- src/Widgets/FormStepContainerWidget/utils/getUrlParamter.ts
+++ src/Widgets/FormStepContainerWidget/utils/getUrlParamter.ts
@@ -1,4 +1,7 @@
 export const getUrlParameter = (parameterKey: string): string => {
+	if (typeof window === "undefined") {
+		return ""
+	}
 	const urlParams = new URLSearchParams(window.location.search);
 	return urlParams.get(parameterKey) || "";
 }
